bb5bfe0a85a37564742129bc920b16677b8645dd
Mathilda <mathilda.magnusson@gmail.com>
2023-08-23 15:59:50 +0200

    done!

--- results-summary-component-main/main.js
+++ results-summary-component-main/main.js
@@ -1,33 +1,40 @@
-async function getJSON() {
-  const response = await fetch("data.json");
-  const categoryContent = await response.json();
-  console.log(categoryContent[1].score);
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const summaryCategories = document.querySelector(".summaryCategories");
 
-categoryElement = document.querySelector("categoryElement");
-getJSON();
+  fetch("data.json") // accesing the json
+    .then((response) => response.json()) // when done fetching, give the content
+    .then((categories) => {
+      // when we have the content we do stuff with it
+      categories.forEach((category) => {
+        createCategory(category);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching JSON:", error);
+    });
 
-let categories = [];
-for (let i = 0; i <= 3; i++) {
-  let category = categoryContent[i];
-  categories.push(category);
-}
+  function createCategory(category) {
+    const categoryElement = document.createElement("article");
+    categoryElement.classList.add("category");
+    summaryCategories.appendChild(categoryElement);
 
-function createCategory(category) {
-  const categoryElement = document.createElement("article");
-  categoryElement.classList.add("category");
-  summaryCategories.appendChild(categoryElement);
+    const categoryIcon = document.createElement("img");
+    categoryIcon.setAttribute("id", "flex1");
+    categoryIcon.src = category.icon;
+    categoryIcon.alt = category.category;
+    categoryElement.appendChild(categoryIcon);
 
-  const categoryIcon = document.createElement("img");
-  categoryIcon.src = category.icon;
-  categoryElement.appendChild(categoryTitle);
+    const categoryTitle = document.createElement("h7");
+    categoryTitle.setAttribute("id", "flex1");
+    categoryTitle.innerText = category.category;
+    categoryTitle.style.color = category.color;
+    categoryElement.appendChild(categoryTitle);
 
-  const categoryTitle = document.createElement("h6");
-  categoryTitle.innerText = category.category;
-  categoryElement.appendChild(categoryTitle);
-
-  const categoryScore = document.createElement("h7");
-  categoryScore.innerText = category.score + "/ 100";
-}
-
-createCategory();
+    const categoryScore = document.createElement("h8");
+    categoryScore.setAttribute("id", "flex2");
+    categoryScore.innerHTML =
+      category.score + '<span id="scoreTransparent"> / 100</span>';
+    categoryElement.appendChild(categoryScore);
+    categoryElement.style.backgroundColor = category.backgroundColor;
+  }
+});
